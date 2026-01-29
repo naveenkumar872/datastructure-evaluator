ca857f9ba5bada83480a3ba1221c62c22014d80d
naveenkumar872 <kumarnaveen54950@gmail.com>
2026-01-29 22:32:03 +0530

    added port

--- server.py
+++ server.py
@@ -250,6 +250,6 @@ def serve_static_file(filename):
 # ============================================
 # Run Server
 # ============================================
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
